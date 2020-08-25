12ae64b28d5e23916b33627f833532cb0f9673a7
Samuel Silva <samus355@gmail.com>
2020-08-25 14:27:44 -0300

    Backend 100%

--- src/controllers/EventsController.ts
+++ src/controllers/EventsController.ts
@@ -22,20 +22,47 @@ export default class EventsController {
     async index(request: Request, response: Response) {
         const filters = request.query;
 
-        if (!filters.dia_da_semana || !filters.estilo || !filters.cidade) {
+        const data = filters.data as string;
+        const style = filters.style as string;
+        const city = filters.city as string;
+
+        if (!filters.data || !filters.style || !filters.city) {
             return response.status(400).json ({
                 error: 'Missing filter to search events'
             });
         }
         
-        const events = await db('class_schedule')
+        const getIdCity = await db('city')
+            .where('city.nome', '=', city)
+            .select('id');
+        
+        const city_id = getIdCity.map((cityItem: StyleItem) => {
+            return cityItem.id
+        });
+
+        const getIdStyle = await db('style')
+            .where('style.nome', '=', style)
+            .select('id');
+
+        const style_id = getIdStyle.map((styleItem: StyleItem) => {
+            return styleItem.id
+        });
+
+        const filtro = await db('events')
+            .where('events.style_id', '=', style_id)
+            .join('schedule', 'events.id', '=', 'schedule.event_id')
+            .where('schedule.city_id', '=', city_id)
+            .whereRaw('`schedule`.`data` = ??', [Number(data)])
+            .select(['schedule.*', 'events.*']);
+
+        /*const events = await db('class_schedule')
             .where('class_schedule.estilo', '=' , filters.estilo as string)
             .andWhere('class_schedule.dia_da_semana', '=' , filters.dia_da_semana as string )
             .andWhere('class_schedule.cidade', '=' , filters.cidade as string)
             .join('events', 'class_schedule.events_id', '=', 'events.id')
-            .select(['events.*', 'class_schedule.*']);
+            .select(['events.*', 'class_schedule.*']);*/
 
-        return response.json(events);
+        return response.json(filtro);
     }
 
     async create(request: Request, response: Response) {
